146c866df397167ea0b27ae26f6506d9f8b02a79
soke4070 <coolexcollins@gmail.com>
2022-09-28 21:44:21 +0100

    Explore Violence Ui

--- lib/pages/explore_page.dart
+++ lib/pages/explore_page.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
+import 'package:flutter/material.dart';
+import 'package:green_app/widgets/custom_explore_app_bar.dart';
 
 class ExplorePage extends StatelessWidget {
   const ExplorePage({super.key});
@@ -7,6 +9,11 @@ class ExplorePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.yellow);
+      color: Colors.black,
+      child: SafeArea(
+          child: CustomScrollView(
+        slivers: [CustomExploreAppBar()],
+      )),
+    );
   }
 }
